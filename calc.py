645a3be819d58f35dc57d68ecbb95b1103acc814
ddori5338 <ddori5338@hanmail.net>
2019-05-31 16:28:56 +0900

    5월말 또 그냥한번해보는

---
+++ calc.py
@@ -0,0 +1,10 @@
+#주피터에서 파일을 빼는건 한 셀 단위로 가능하다.
+
+def calc_sum(a,b):
+    return a+b
+
+def calc_mul(a,b):
+    return a*b
+
+def calc_sub(a,b):
+    return a-b
---
+++ mypackage/calc_modules.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 31 11:29:12 2019
+
+@author: Funzo
+"""
+
+def sum_calc(a,b):
+    return a+b
+
+class Calculator(object):
+    pass
